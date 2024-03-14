88b01bf6fba3e6c0aef10030344335932a34d127
chiril200 <ionhawk397@gmail.com>
2024-03-14 23:49:32 +0200

    init

---
+++ date_create.py
@@ -0,0 +1,16 @@
+def name_data(): 
+    name = input('Ведите ваше имя:')
+    print('Очень красивоя имя')
+    return name
+
+def surname_data():
+    surname = input('Ведите ваше Фамилию:')
+    return surname 
+
+def phone_data():
+    phone = input('Ведите ваше Телефон:')
+    return phone 
+
+def address_data():
+    address = input('Ведите ваше адрес:')
+    return address 
